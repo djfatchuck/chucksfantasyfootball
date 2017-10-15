1826e1f664f840ad066cb441530aab926f0c551a
charles <charles.schrumpf@kiswe.com>
2017-10-15 12:50:13 -0400

    add bootstrap locally

--- chucksfantasyfootball/settings.py
+++ chucksfantasyfootball/settings.py
@@ -126,7 +126,7 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
